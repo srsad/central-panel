9daed8d01e01611c7c1966e15700bd5dc67a19c6
SRS AD <artaksergievich@gmail.com>
2020-03-05 00:27:45 +0300

    feature: подключил vuesax

---
+++ plugins/globals.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import Vuesax from 'vuesax'
+
+import 'vuesax/dist/vuesax.css'
+Vue.use(Vuesax, {
+  server: true,
+  theme: {
+    colors: {
+      primary: '#5b3cc4',
+      success: 'rgb(23, 201, 100)',
+      danger: 'rgb(242, 19, 93)',
+      warning: 'rgb(255, 130, 0)',
+      dark: 'rgb(36, 33, 69)'
+    }
+  }
+})
