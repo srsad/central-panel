d2c74e3fb486d8cc5207ebfed5212e6faea13bd6
SRS AD <artaksergievich@gmail.com>
2020-03-07 12:54:30 +0300

    feature: (Breadcrumb.vue)

---
+++ components/common/Breadcrumb.vue
@@ -0,0 +1,7 @@
+<template>
+  <el-breadcrumb separator="/">
+    <el-breadcrumb-item :to="{ path: '/' }">Главная</el-breadcrumb-item>
+    <el-breadcrumb-item to="/report">Вложенность 2</el-breadcrumb-item>
+    <el-breadcrumb-item>Вложенность 3</el-breadcrumb-item>
+  </el-breadcrumb>
+</template>
