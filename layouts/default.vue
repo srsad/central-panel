7289d3cb7199e9d1b7c156e73367191e82d8e52c
SRS AD <artaksergievich@gmail.com>
2020-03-07 12:55:35 +0300

    update: (default.vue) подобрал основную структуру

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,23 +1,39 @@
 <template>
-  <div>
-    <el-row type="flex">
-      <div>
-        <navbar />
-      </div>
-      <div>
-        <nuxt />
-      </div>
-    </el-row>
-  </div>
+  <el-row type="flex">
+    <el-aside style="width:auto">
+      <sidebar />
+    </el-aside>
+    <el-container>
+      <el-header class="main-header">
+        <el-row type="flex" class="w100">
+          <el-col :span="18" class="align-items-center">
+            <breadcrumb />
+          </el-col>
+          <el-col :span="6" class="text-right">
+            <navbar />
+          </el-col>
+        </el-row>
+      </el-header>
+      <el-row type="flex" class="w100">
+        <el-col :span="24">
+          <nuxt />
+        </el-col>
+      </el-row>
+    </el-container>
+  </el-row>
 </template>
 
 <script>
+import Sidebar from '~/components/common/Sidebar'
 import Navbar from '~/components/common/Navbar'
+import Breadcrumb from '~/components/common/Breadcrumb'
 
 export default {
   // midleware: ['auth']
   components: {
-    Navbar
+    Sidebar,
+    Navbar,
+    Breadcrumb
   }
 }
 </script>
