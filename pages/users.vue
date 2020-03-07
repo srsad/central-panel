9105308b1b554ae79cf4209ceb8245dd2a67e939
SRS AD <artaksergievich@gmail.com>
2020-03-07 21:16:56 +0300

    update: (users.vue) подключил панели ролей и пользователей

--- pages/users.vue
+++ pages/users.vue
@@ -1,9 +1,24 @@
 <template>
   <el-container>
-    me
+    <el-tabs value="users" class="w100">
+      <el-tab-pane label="Пользователи" name="users">
+        <app-users-panel />
+      </el-tab-pane>
+      <el-tab-pane label="Роли" name="roles">
+        <app-roles-panel />
+      </el-tab-pane>
+    </el-tabs>
   </el-container>
 </template>
 
 <script>
-export default {}
+import AppRolesPanel from '~/components/users/roles/Panel'
+import AppUsersPanel from '~/components/users/users/Panel'
+
+export default {
+  components: {
+    AppRolesPanel,
+    AppUsersPanel
+  }
+}
 </script>
