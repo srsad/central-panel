84895a7a319eac51d7ac470a6f40eb060b1ae542
SRS AD <artaksergievich@gmail.com>
2020-03-07 17:38:27 +0300

    update: (index.vue) накидал первый экран

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,31 @@
 <template>
   <div class="row">
+    <div class="col-6 text-center">
+      <button class="mb-20" style="padding:0;cursor:default">
+        <img src="/site/images/rserv.jpg" alt="Rservice" style="height:90px" />
+      </button>
+      <p class="text-center">
+        Санкт-Петербург, 2-я Красноармейская 11
+        <br />
+        Краснодар, ул. Пашковская 83
+      </p>
+    </div>
+    <div class="col-6 text-center">
+      <button class="mb-20" style="padding:0;cursor:default">
+        <img src="/site/images/imp.jpg" alt="Rservice" style="height:90px" />
+      </button>
+      <p class="text-center">Санкт-Петербург, ул. Кирочная, д.29<br /></p>
+    </div>
+    <!--  -->
     <div class="col-6">
-      1
+      <div class="row">
+        <div class="col-12">список 1</div>
+      </div>
     </div>
     <div class="col-6">
-      2
+      <div class="row">
+        <div class="col-12">список 2</div>
+      </div>
     </div>
   </div>
 </template>
