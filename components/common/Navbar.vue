63df942c45cae745999f9c28747f0c6ba393d76e
SRS AD <artaksergievich@gmail.com>
2020-03-07 12:53:18 +0300

    feature: (Navbar.vue) верхняя навигация

--- components/common/Navbar.vue
+++ components/common/Navbar.vue
@@ -1,93 +1,34 @@
 <template>
-  <div>
-    <el-menu
-      :collapse="$store.state.settings.isCollapse"
-      class="el-menu-vertical"
-      background-color="#304156"
-      text-color="#bfcbd9"
-      active-text-color="#ffd04b"
-    >
-      <!-- content -->
-      <!-- end content -->
-      <el-menu-item
-        @click="
-          $store.dispatch(
-            'settings/switchIsCollapse',
-            !$store.state.settings.isCollapse
-          )
-        "
-        index="expand"
-        class="color-fff"
-      >
-        <i class="el-icon-sort color-fff rotate-90" />
-        <span slot="title">
-          {{ $store.state.settings.isCollapse ? 'Открыть' : 'Скрыть' }}
-        </span>
-      </el-menu-item>
-      <el-submenu index="companies">
-        <template slot="title">
-          <i class="fa fa-database" />
-          <span slot="title">Компании</span>
-        </template>
-        <!--  -->
-        <el-menu-item-group>
-          <el-submenu index="rservice">
-            <span slot="title">Rservice</span>
-            <el-menu-item index="Canon1">Canon</el-menu-item>
-            <el-menu-item index="Nicon1">Nicon</el-menu-item>
-          </el-submenu>
-          <el-submenu index="impuls">
-            <span slot="title">Impuls</span>
-            <el-menu-item index="Apple2">Apple</el-menu-item>
-            <el-menu-item index="MSI2">MSI</el-menu-item>
-          </el-submenu>
-        </el-menu-item-group>
-        <!--  -->
-      </el-submenu>
-
-      <el-submenu index="2-1">
-        <span slot="title">Rservice</span>
-        <el-menu-item index="2-1-1">Canon</el-menu-item>
-        <el-menu-item index="2-1-2">Nicon</el-menu-item>
-      </el-submenu>
-      <el-submenu index="2-2">
-        <span slot="title">Impuls</span>
-        <el-menu-item index="2-2-1">Apple</el-menu-item>
-        <el-menu-item index="2-2-2">MSI</el-menu-item>
-      </el-submenu>
-
-      <el-menu-item index="files">
-        <i class="fa fa-file-excel-o" />
-        <span slot="title">Отчеты</span>
-      </el-menu-item>
-      <el-menu-item index="map">
-        <i class="fa fa-map-o" />
-        <span slot="title">Тепловые карты</span>
-      </el-menu-item>
-      <el-menu-item index="users">
-        <i class="fa fa-users" />
-        <span slot="title">Пользователи</span>
-      </el-menu-item>
-      <!--  -->
-      <el-menu-item index="settings">
-        <i class="el-icon-setting" />
-        <span slot="title">Настройки</span>
-      </el-menu-item>
-      <!-- TODO в разделе "Настройки" будем кранить логи деталей -->
-      <!--  -->
-      <el-menu-item index="sign-out">
-        <i class="fa fa-sign-out" />
-        <span slot="title">Выход</span>
-      </el-menu-item>
-    </el-menu>
-  </div>
+  <el-dropdown @command="handleCommand" trigger="click">
+    <el-button type="primary" size="mini">
+      ФИО Сотрудника
+      <i class="el-icon-arrow-down el-icon--right"></i>
+    </el-button>
+    <el-dropdown-menu slot="dropdown" router>
+      <el-dropdown-item command="/profile" icon="fa fa-user-o">
+        Мой профиль
+      </el-dropdown-item>
+      <el-dropdown-item command="logout" icon="fa fa-sign-out" divided>
+        Выход
+      </el-dropdown-item>
+    </el-dropdown-menu>
+  </el-dropdown>
 </template>
 
-<style>
-.el-menu-vertical {
-  min-height: 100vh;
+<script>
+export default {
+  methods: {
+    handleCommand(data) {
+      if (data === 'logout') {
+        this.logout()
+      } else {
+        // console.log(data)
+        this.$router.push(data)
+      }
+    },
+    logout() {
+      console.log('logout')
+    }
+  }
 }
-.el-menu-vertical:not(.el-menu--collapse) {
-  width: 200px;
-}
-</style>
+</script>
