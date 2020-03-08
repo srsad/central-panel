21205ab67ca64e05c7adbac6092d5de01318b90e
SRS AD <artaksergievich@gmail.com>
2020-03-08 10:56:12 +0300

    added: (apiKey.model.js) модель для генерации API ключей

---
+++ server/models/apiKey.model.js
@@ -0,0 +1,31 @@
+/**
+ * API ключи
+ */
+
+const { model, Schema } = require('mongoose')
+
+const apiKeySchema = new Schema({
+  comments: {
+    type: String,
+    maxlength: 255,
+    required: true
+  },
+  key: {
+    type: String,
+    maxlength: 64,
+    required: true
+  },
+  created: {
+    type: Date,
+    default: Date.now
+  },
+  updated: {
+    type: Date
+  },
+  user_id: {
+    type: Schema.Types.ObjectId,
+    ref: 'users'
+  }
+})
+
+module.exports = model('api_keys', apiKeySchema)
