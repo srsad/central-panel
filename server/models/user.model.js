94b6b0830ea6b767ce83dd85a07b33d988b1eb68
SRS AD <artaksergievich@gmail.com>
2020-03-08 10:57:41 +0300

    added: (user.model.js) схема пользователей

---
+++ server/models/user.model.js
@@ -0,0 +1,57 @@
+/**
+ * Пользователи
+ */
+
+const { model, Schema } = require('mongoose')
+
+const userSchema = new Schema({
+  login: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  fullname: {
+    type: String
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  description: {
+    type: String,
+    maxlength: 255
+  },
+  phone: {
+    type: Number,
+    unique: true,
+    required: true,
+    maxlength: 32
+  },
+  active: {
+    type: Boolean,
+    required: true
+  },
+  avatar: {
+    type: String
+  },
+  // blockeduntil - заблокирован до (дата)
+  // blockedafter - заблокирован после (дата)
+  role: {
+    type: Schema.Types.ObjectId,
+    ref: 'roles'
+  },
+  created: {
+    type: Date,
+    default: Date.now
+  },
+  updated: {
+    type: Date
+  }
+})
+
+module.exports = model('users', userSchema)
