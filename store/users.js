de664c593e6f73d72bec4b82695a3d211ed824c6
SRS AD <artaksergievich@gmail.com>
2020-03-08 23:17:45 +0300

    update: (store/users.js) почти полностью переписал

--- store/users.js
+++ store/users.js
@@ -3,14 +3,35 @@
  */
 
 export const state = () => ({
-  users: []
+  users: [], // список пользователей
+  roles: [], // список ролей
+  user: [], // пользователь для редактирования
+  role: [] // роль для редактирования
 })
 
 export const actions = {
+  async createRole({ commit }, formData) {
+    // TODO вставлять в запрос userID
+    try {
+      await this.$axios.$post('/api/v1/role/create', formData)
+    } catch (e) {
+      commit('SET_ERROR', e.response.data.message, { root: true })
+      throw e
+    }
+  },
+  async fetchRoles({ commit }) {
+    try {
+      const roles = await this.$axios.$get('/api/v1/role/getall')
+      commit('SET_ROLES', roles.data)
+    } catch (e) {
+      commit('SET_ERROR', e.response.data.message, { root: true })
+      throw e
+    }
+  },
   updateUser({ commit }, formData) {
     commit('UPDATE_USER', formData)
   },
-  loadUsers({ commit }, params) {
+  fetchUsers({ commit }, params) {
     const users = []
     commit('LOAD_USERS', users)
   }
@@ -18,7 +39,23 @@ export const actions = {
 
 export const mutations = {
   UPDATE_USER(state, formData) {},
-  LOAD_USERS(state, users) {
+  SET_USERS(state, users) {
     state.users = users
+  },
+  SET_ROLES(state, roles) {
+    state.roles = roles
+  },
+  SET_USER(state, user) {
+    state.user = user
+  },
+  SET_ROLE(state, role) {
+    state.role = role
   }
+}
+
+export const getters = {
+  users: (state) => state.users,
+  roles: (state) => state.roles,
+  user: (state) => state.user[0],
+  role: (state) => state.role[0]
 }
