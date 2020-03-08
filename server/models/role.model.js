db42669cfb1b3b6b4f4ff4c9b268fe905a2ed97b
SRS AD <artaksergievich@gmail.com>
2020-03-08 10:56:50 +0300

    added: (role.model.js) роли пользователей

---
+++ server/models/role.model.js
@@ -0,0 +1,30 @@
+/**
+ * Роли пользователей
+ */
+
+const { model, Schema } = require('mongoose')
+
+const roleSchema = new Schema({
+  name: {
+    type: String,
+    maxlength: 255,
+    required: true
+  },
+  access: {
+    type: Array,
+    default: [] // нет доступов ни к одному из защищенных разделов
+  },
+  created: {
+    type: Date,
+    default: Date.now
+  },
+  updated: {
+    type: Date
+  },
+  user_id: {
+    type: Schema.Types.ObjectId,
+    ref: 'users'
+  }
+})
+
+module.exports = model('roles', roleSchema)
