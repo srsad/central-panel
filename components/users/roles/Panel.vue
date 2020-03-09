43624427207a5910a865442be0ff9212978545cc
SRS AD <artaksergievich@gmail.com>
2020-03-10 01:12:36 +0300

    update: (role/Panel.vue) подключил элементы для ролей

--- components/users/roles/Panel.vue
+++ components/users/roles/Panel.vue
@@ -1,7 +1,12 @@
 <template>
   <div>
     <el-button
-      @click="drawer = true"
+      @click="
+        $store.commit('settings/SWITCH_DRAWNER', {
+          dranwer: 'drawerCreate',
+          status: true
+        })
+      "
       type="success"
       class="mb-20"
       icon="el-icon-plus"
@@ -9,28 +14,23 @@
       Добаивть роль
     </el-button>
     <!--  -->
-    <app-list />
+    <app-list :items="$store.getters['users/roles']" />
     <!--  -->
-    <app-drawer
-      :drawer="drawer"
-      @closeDrawner="(status) => (drawer = status)"
-    />
+    <app-drawer-create />
+    <app-drawer-update />
   </div>
 </template>
 
 <script>
-import AppDrawer from '~/components/users/roles/Drawer'
+import AppDrawerCreate from '~/components/users/roles/DrawerCreate'
+import AppDrawerUpdate from '~/components/users/roles/DrawerUpdate'
 import AppList from '~/components/users/roles/List'
 
 export default {
   components: {
-    AppDrawer,
+    AppDrawerCreate,
+    AppDrawerUpdate,
     AppList
-  },
-  data() {
-    return {
-      drawer: false
-    }
   }
 }
 </script>
