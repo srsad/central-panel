8c71fdb5a6972d43ab963d7784d8ce7be04018ab
SRS AD <artaksergievich@gmail.com>
2020-03-10 10:12:39 +0300

    update: (layouts/auth.vue) добавил обработку ошибок

--- layouts/auth.vue
+++ layouts/auth.vue
@@ -4,6 +4,21 @@
 
 <script>
 export default {
-  //
+  computed: {
+    error() {
+      return this.$store.getters.error
+    }
+  },
+  watch: {
+    async error(value) {
+      if (value === null) return
+      await this.$notify({
+        title: 'Ошибка',
+        message: value,
+        customClass: 'error-notyfy'
+      })
+      this.$store.commit('CLEAR_ERROR')
+    }
+  }
 }
 </script>
