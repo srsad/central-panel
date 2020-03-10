300ca940e0e2aebec66239643e980d68fbc15dff
SRS AD <artaksergievich@gmail.com>
2020-03-10 23:58:31 +0300

    update: (user/List.vue) подключил таблицу с пользователями

--- components/users/users/List.vue
+++ components/users/users/List.vue
@@ -1,25 +1,54 @@
 <template>
-  <el-table :data="tableData" empty-text="Нет данных" stripe class="w100">
+  <el-table
+    :data="items"
+    @row-dblclick="edit"
+    empty-text="Нет данных"
+    stripe
+    class="w100"
+  >
     <el-table-column prop="login" label="Логин" />
     <el-table-column prop="fullname" label="ФИО" />
     <el-table-column prop="email" label="Email" />
-    <el-table-column prop="role" label="Роль" />
-    <el-table-column prop="date" label="Дата создания" />
+    <el-table-column prop="role.name" label="Роль" />
+    <el-table-column prop="created" label="Дата создания" />
     <el-table-column label="Действия" label-class-name="text-center">
       <template slot-scope="scope">
         <div class="text-center">
-          <el-button
-            @click="handleEdit(scope.$index, scope.row)"
-            size="mini"
-            icon="el-icon-edit"
-          />
-
-          <el-button
-            @click="handleDelete(scope.$index, scope.row)"
-            size="mini"
-            type="danger"
-            icon="el-icon-delete"
-          />
+          <el-button-group>
+            <el-button
+              @click="edit(scope.row)"
+              :loading="loading"
+              size="mini"
+              type="primary"
+              icon="el-icon-edit"
+            />
+            <el-button
+              @click="switchActive(scope.row)"
+              :icon="`${scope.row.active ? 'el-icon-unlock' : 'el-icon-lock'}`"
+              :title="
+                `${scope.row.active ? 'Заблокировать' : 'Разблокировать'}`
+              "
+              :loading="loading"
+              size="mini"
+              type="info"
+            />
+            <el-popconfirm
+              @onConfirm="remove(scope.$index, scope.row)"
+              title="Удалить пользователя?"
+              confirm-button-text="Да"
+              confirm-button-type="success"
+              cancel-button-type="default"
+              cancel-button-text="Нет, спасибо"
+            >
+              <el-button
+                slot="reference"
+                :loading="loading"
+                size="mini"
+                type="danger"
+                icon="el-icon-delete"
+              />
+            </el-popconfirm>
+          </el-button-group>
         </div>
       </template>
     </el-table-column>
@@ -28,49 +57,60 @@
 
 <script>
 export default {
+  props: {
+    items: {
+      type: Array,
+      default: () => []
+    }
+  },
   data() {
     return {
-      tableData: [
-        {
-          login: 'Login 1',
-          fullname: 'Tom 1',
-          email: 'email1@gmail.com',
-          role: 'Роль 1',
-          date: '2016-05-03'
-        },
-        {
-          login: 'Login 2',
-          fullname: 'Tom 2',
-          email: 'email2@gmail.com',
-          role: 'Роль 2',
-          date: '2016-05-02'
-        },
-        {
-          login: 'Login 3',
-          fullname: 'Tom 3',
-          email: 'email3@gmail.com',
-          role: 'Роль 3',
-          date: '2016-05-04'
-        },
-        {
-          login: 'Login 4',
-          fullname: 'Tom 4',
-          email: 'email4@gmail.com',
-          role: 'Роль 4',
-          date: '2016-05-01'
-        }
-      ]
+      loading: false
     }
   },
   methods: {
-    handleClick() {
-      console.log('click')
+    async remove(idx, item) {
+      this.loading = true
+      try {
+        await this.$axios.$delete('/api/v1/user/remove/' + item._id)
+        this.$store.dispatch('users/fetchUsers')
+        this.$notify({
+          message: 'Пользователь успушно удален!',
+          customClass: 'success-notyfy'
+        })
+      } catch (e) {
+        this.$store.commit('SET_ERROR', e.response.data.message)
+        throw e
+      } finally {
+        this.loading = false
+      }
     },
-    handleEdit(index, row) {
-      console.log(index, row)
+    edit(item) {
+      if (this.loading === true) return
+      this.$store.commit('users/SET_USER', item)
+      this.$store.commit('settings/SWITCH_DRAWNER', {
+        dranwer: 'drawerUpdateUser',
+        status: true
+      })
     },
-    handleDelete(index, row) {
-      console.log(index, row)
+    async switchActive(item) {
+      this.loading = true
+      try {
+        await this.$axios.$post('/api/v1/user/switch-active/' + item._id, {
+          active: !item.active
+        })
+        this.$store.dispatch('users/fetchUsers')
+        this.$notify({
+          // eslint-disable-next-line prettier/prettier
+          message: `Пользователь ${item.active ? 'заблокирован' : 'разблокирован'}!`,
+          customClass: 'success-notyfy'
+        })
+      } catch (e) {
+        this.$store.commit('SET_ERROR', e.response.data.message)
+        throw e
+      } finally {
+        this.loading = false
+      }
     }
   }
 }
