ac3b4395b7c74324eca9ee3eaed551e2fe9e559d
SRS AD <artaksergievich@gmail.com>
2020-03-19 00:53:10 +0300

    feature: (domain.model.js) модель для доменов

---
+++ server/models/domain.model.js
@@ -0,0 +1,50 @@
+/**
+ * Модель доменных имен
+ */
+
+const { model, Schema } = require('mongoose')
+
+const domainSchema = new Schema({
+  name: {
+    type: String,
+    maxlength: 255,
+    index: true
+  },
+  brand: {
+    type: String,
+    maxlength: 255,
+    index: true
+  },
+  domain: {
+    type: String,
+    maxlength: 255,
+    required: true,
+    index: true
+  },
+  description: {
+    type: String,
+    maxlength: 255
+  },
+  vendor: {
+    type: String,
+    maxlength: 255,
+    index: true
+  },
+  ststus: {
+    type: Boolean,
+    default: true
+  },
+  color: {
+    type: String,
+    maxlength: 16
+  },
+  created: {
+    type: Date,
+    default: Date.now
+  },
+  updated: {
+    type: Date
+  }
+})
+
+module.exports = model('domains', domainSchema)
