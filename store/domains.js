c3bf64b7b0f06ec7f4e71b29456765b300aabe7d
SRS AD <artaksergievich@gmail.com>
2020-03-19 00:51:01 +0300

    feature: (store/domains.js) стор для раздела с доменами

---
+++ store/domains.js
@@ -0,0 +1,51 @@
+/**
+ * Стор для списка доменных имен
+ */
+
+export const state = () => ({
+  damains: [], // список доменных имен
+  damain: null // доменное имя для редактирования
+})
+
+export const actions = {
+  async createDomain({ commit }, formData) {
+    try {
+      await this.$axios.$post('/api/v1/domain/create', formData)
+    } catch (e) {
+      commit('SET_ERROR', e.response.data.message, { root: true })
+      throw e
+    }
+  },
+  async updateDomain({ commit }, formData) {
+    try {
+      await this.$axios.$post('/api/v1/domain/update/' + formData._id, formData)
+      // commit('SET_DOMAINS', [])
+    } catch (e) {
+      commit('SET_ERROR', e.response.data.message, { root: true })
+      throw e
+    }
+  },
+  async fetchDomains({ commit }) {
+    try {
+      const domains = await this.$axios.$get('/api/v1/domain/getall')
+      commit('SET_DOMAINS', domains.data)
+    } catch (e) {
+      commit('SET_ERROR', e.response.data.message, { root: true })
+      throw e
+    }
+  }
+}
+
+export const mutations = {
+  SET_DOMAINS(state, roles) {
+    state.roles = roles
+  },
+  SET_DOMAIN(state, role) {
+    state.role = role
+  }
+}
+
+export const getters = {
+  domains: (state) => state.damains,
+  domain: (state) => state.damain
+}
