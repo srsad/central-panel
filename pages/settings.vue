bafad88e7337ab5e2400a186d9346ea7fa5f93bc
SRS AD <artaksergievich@gmail.com>
2020-03-19 23:31:38 +0300

    update: (settings.vue) подключил список пользователей

--- pages/settings.vue
+++ pages/settings.vue
@@ -3,7 +3,7 @@
     <div class="col-12">
       <el-tabs value="apikey">
         <el-tab-pane label="API ключи" name="apikey">
-          <AppApiKey :items="items" />
+          <AppApiKey />
         </el-tab-pane>
         <el-tab-pane label="Два" name="second">Два</el-tab-pane>
         <el-tab-pane label="Три" name="third">Три</el-tab-pane>
@@ -18,33 +18,6 @@ import AppApiKey from '~/components/settings/ApiKeys'
 export default {
   components: {
     AppApiKey
-  },
-  data() {
-    return {
-      items: [
-        {
-          _id: '_idasdkasdkasd',
-          comments: 'Комментарий 1',
-          key: '7a62c3ecf4fb8b3f3f956341e8f43ac8',
-          created: 'дата создания',
-          updated: 'дата обновления'
-        },
-        {
-          _id: '_idasdkasdkasd',
-          comments: 'Комментарий 2',
-          key: 'a230ce13cbffd20d6dfdde7a9786c293',
-          created: 'дата создания',
-          updated: 'дата обновления'
-        },
-        {
-          _id: '_idasdkasdkasd',
-          comments: 'Комментарий 3',
-          key: '3484db6e3de76e9281a13910f37dd0f3',
-          created: 'дата создания',
-          updated: 'дата обновления'
-        }
-      ]
-    }
   }
 }
 </script>
