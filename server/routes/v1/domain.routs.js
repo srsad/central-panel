0b4dc443b9b3f5accba4fedbf933032cf142adc4
SRS AD <artaksergievich@gmail.com>
2020-03-19 22:12:16 +0300

    fix: (domain.routs.js) переименовал + fix update метод + удалил busboy-body-parser

--- server/routes/v1/domain.routs.js
+++ server/routes/v1/domain.routs.js
@@ -1,5 +1,3 @@
-const busboyBodyParser = require('busboy-body-parser')
-
 const {
   create,
   update,
@@ -14,8 +12,8 @@ const router = Router()
 // /api/v1/domain
 router.get('/getall', getAll)
 router.get('/get/:id', getById)
-router.post('/create', busboyBodyParser(), create)
-router.post('/update', busboyBodyParser(), update)
+router.post('/create', create)
+router.post('/update/:id', update)
 router.delete('/remove/:id', remove)
 
 module.exports = router
