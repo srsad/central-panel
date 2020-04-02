80964cfd74c64ad36dea03d0d03c384ac3997c9b
SRS AD <artaksergievich@gmail.com>
2020-04-02 12:32:47 +0300

    update: (sourceSite.js) удалил события создания/обновления и домолнил геттеры

--- store/sourceSite.js
+++ store/sourceSite.js
@@ -8,23 +8,6 @@ export const state = () => ({
 })
 
 export const actions = {
-  async create({ commit }, formData) {
-    try {
-      await this.$axios.$post('/api/v1/source-site/create', formData)
-    } catch (e) {
-      commit('SET_ERROR', e.response.data.message, { root: true })
-      throw e
-    }
-  },
-  async update({ commit }, formData) {
-    try {
-      // eslint-disable-next-line prettier/prettier
-      await this.$axios.$post('/api/v1/source-site/update/' + formData._id, formData)
-    } catch (e) {
-      commit('SET_ERROR', e.response.data.message, { root: true })
-      throw e
-    }
-  },
   async fetchItems({ commit }) {
     try {
       const domains = await this.$axios.$get('/api/v1/source-site/getall')
@@ -47,5 +30,39 @@ export const mutations = {
 
 export const getters = {
   sources: (state) => state.sources,
-  source: (state) => state.source
+  source: (state) => state.source,
+  /**
+   * Возвращает отсортированный по компаниям список
+   * sources: {
+   *    rservice: [
+   *      { name: 'Apple 1', _id: 1 ...},
+   *      ...
+   *    ],
+   *    impuls: [
+   *      { name: 'MSI 5', _id: 5 ...},
+   *      ...
+   *    ],
+   *    ...
+   *  }
+   */
+  sortSources: (state) => {
+    const res = {}
+    for (const item of state.sources) {
+      if (!res.hasOwnProperty(item.company)) res[item.company] = []
+      res[item.company].push(item)
+    }
+    // надоо обязательно проверить, существует ли одна из обязательных компаний, если нет, то надо создать
+    // чтоб не возникало ошибки, когда мы перетащили все бренды в одну и не можем вернуть что-то обратно в пустую компанию
+    if (!res.impuls) res.impuls = []
+    if (!res.rservice) res.rservice = []
+    return res
+  },
+  /** Возвращает только уникальный список текущих бренодов */
+  sourcesName: (state) => {
+    const brands = state.sources.map((el) => el.brand)
+    const res = [...new Set(brands)].map((el) => {
+      return { value: el }
+    })
+    return res
+  }
 }
