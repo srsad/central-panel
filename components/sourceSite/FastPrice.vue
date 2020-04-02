de5b810301af9f50a205f80e85b6f3471ab1882b
SRS AD <artaksergievich@gmail.com>
2020-04-02 23:28:43 +0300

    added: (FastPrice.vue) быстрый прайс неисправностей

---
+++ components/sourceSite/FastPrice.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <el-table :data="fastPrice" stripe style="width: 100%">
+      <el-table-column prop="pTitle" label="Наименование">
+        <template slot-scope="scope">
+          <b class="color-000">{{ scope.row.pTitle }} мин.</b>
+        </template>
+      </el-table-column>
+      <el-table-column prop="pTime" label="Врем ремонта">
+        <template slot="header">
+          <div class="cell text-center">Врем ремонта</div>
+        </template>
+        <template slot-scope="scope">
+          <div class="text-center">{{ scope.row.pTime }} мин.</div>
+        </template>
+      </el-table-column>
+      <el-table-column prop="pPrice" label="Цена">
+        <template slot-scope="scope">
+          <div>{{ scope.row.pPrice }} руб.</div>
+        </template>
+      </el-table-column>
+    </el-table>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    fastPrice() {
+      return this.$store.getters['source/page/fastPrice']
+    }
+  }
+}
+</script>
