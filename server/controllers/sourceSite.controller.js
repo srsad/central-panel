84086fe5b9b9564d826adc1c870c15bd91c33017
SRS AD <artaksergievich@gmail.com>
2020-04-02 12:30:13 +0300

    update: (sourceSite.controller.js) + загрузка картинок поправил удаление источника, сортировка происходиит по { brand: 1 } Удалил лишний метод getNextSequence

--- server/controllers/sourceSite.controller.js
+++ server/controllers/sourceSite.controller.js
@@ -1,3 +1,6 @@
+const path = require('path')
+const moment = require('moment')
+const { imageLoader, rimraf } = require('../utils/imageUploader')
 const SSite = require('../models/sourceSite.model')
 
 /** Создание */
@@ -7,16 +10,31 @@ module.exports.create = async (req, res) => {
     delete formData.image
     // formData.menuindex = await getNextSequence()
     formData.menuindex = 0
-    const sourceSite = await new SSite(formData)
-    sourceSite.save()
-    if (req.body.image) {
-      console.log('save image')
+    const sourceSite = new SSite(formData)
+    await sourceSite.save()
+    // если еcть картинка
+    if (req.files.image) {
+      let logoPath = ''
+      const basePath = '../../static/site/images/brand/'
+      const fileName = `${moment().format('YYYY-MM-DD-HHmmss_SSS')}-${
+        req.files.image.name
+      }`
+      logoPath = await imageLoader({
+        basePath,
+        fileName,
+        filePath: `/${sourceSite._id}/${fileName}`,
+        savePath: `/${sourceSite._id}/`, // '../../static/site/images/brand/' + ${sourceSite._id}
+        bufferImage: req.files.image.data
+      })
+      const $set = {}
+      if (logoPath !== '') $set.image = logoPath
+      await SSite.updateOne({ _id: sourceSite._id }, { $set }, { new: true })
     }
     res.status(201).json({ message: 'Источник создан!' })
   } catch (error) {
     let msg = ''
-    if (error.errmsg.includes('$domain_1')) {
-      msg = ' Доменное имя уже существует!'
+    if (error.errmsg.includes('$source_1')) {
+      msg = ' Доменное имя уже привязанно!'
     }
     res.status(409).json({ message: 'Источник не добавлен!' + msg, error })
   }
@@ -26,20 +44,43 @@ module.exports.create = async (req, res) => {
 module.exports.update = async (req, res) => {
   const $set = req.body
   try {
-    await SSite.findOneAndUpdate(
-      { _id: req.params.id },
-      { $set },
-      { new: true }
-    )
+    let logoPath = ''
+    // если еcть картинка
+    if (req.files.image) {
+      const basePath = '../../static/site/images/brand/'
+      const fileName = `${moment().format('YYYY-MM-DD-HHmmss_SSS')}-${
+        req.files.image.name
+      }`
+
+      // удаляем папку со старой картинкой
+      rimraf(path.resolve(__dirname, basePath + req.params.id))
+      // грузим новую
+      logoPath = await imageLoader({
+        basePath,
+        fileName,
+        filePath: `/${req.params.id}/${fileName}`,
+        savePath: `/${req.params.id}/`, // '../../static/site/images/brand/' + ${req.params.id}
+        bufferImage: req.files.image.data
+      })
+    }
+    if (logoPath !== '') $set.image = logoPath
+    await SSite.updateOne({ _id: req.params.id }, { $set }, { new: true })
     res.json({ message: 'Данные обновленны!' })
   } catch (error) {
-    res.status(500).json({ message: 'Не удалось обновить данные!', error })
+    let msg = ''
+    if (error.errmsg.includes('$source_1')) {
+      msg = ' Доменное имя уже привязанно!'
+    }
+    res.status(409).json({ message: 'Источник не добавлен!' + msg, error })
   }
 }
 
 /** Удаление */
 module.exports.remove = async (req, res) => {
   try {
+    // удаляем папку с картинкой
+    // eslint-disable-next-line prettier/prettier
+    rimraf(path.resolve(__dirname, '../../static/site/images/brand/' + req.params.id))
     await SSite.deleteOne({ _id: req.params.id })
     res.status(200).json({ message: 'Источник удален!' })
   } catch (error) {
@@ -60,7 +101,7 @@ module.exports.getById = async (req, res) => {
 /** Вернуть весь список */
 module.exports.getAll = async (req, res) => {
   try {
-    const domains = await SSite.find().sort({ created: -1 })
+    const domains = await SSite.find().sort({ brand: 1 })
     res.json({ data: domains })
   } catch (error) {
     res.status(500).json({ message: 'Не удалось получить список источников!' })
