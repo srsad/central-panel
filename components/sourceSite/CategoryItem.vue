5c7e5a446f3f9fd168a916056bfaab276ac530f4
SRS AD <artaksergievich@gmail.com>
2020-04-02 16:36:59 +0300

    added: (CategoryItem.vue) карточка элемента категорий

---
+++ components/sourceSite/CategoryItem.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="category">
+    <div
+      v-if="item.tv_imgCategiry !== ''"
+      :style="
+        `
+        background: url(https://${$store.state.source.page.params.source}${item.tv_imgCategiry}) 50% 50% / contain no-repeat;
+      `
+      "
+      class="category__img"
+    />
+    <div class="category__text">
+      {{ item.pagetitle }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    item: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
