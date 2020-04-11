637339853cf319af27dec811c41a906e78cc25ea
SRS AD <artaksergievich@gmail.com>
2020-04-12 00:05:27 +0300

    update: (part.model.js) изменил тип поля brand, category + excepts - список исключений + description - описание

--- server/models/part.model.js
+++ server/models/part.model.js
@@ -5,31 +5,42 @@
 const { model, Schema } = require('mongoose')
 
 const part = new Schema({
+  // наименование детали
   name: {
     type: String,
     maxlength: 255,
     required: true,
     index: true
   },
-  brands: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: 'source_site'
-    }
-  ],
-  service: {
-    type: String
+  // бренда по toLowerCase
+  brand: {
+    type: String,
+    maxlength: 255,
+    required: true
   },
+  // категория к которой привязана деталь (смартфоны/ноутбуки/моноблоки и т.д.)
+  category: {
+    type: Array
+  },
+  // цена детали
   price: {
     type: String // Иногда может встречаться текст -_-
   },
+  // цена ремонта impuls
   impuls: {
     type: String
   },
+  // цена ремонта rservice
   rservice: {
     type: String
   },
-  datas: {
+  // описание
+  description: {
+    type: String,
+    maxlength: 255
+  },
+  // список исключений, намеренно не назвал exceptions
+  excepts: {
     type: Array,
     default: []
   },
