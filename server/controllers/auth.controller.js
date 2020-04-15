a3daaeab029a3e47c3b78dbd3fae2594e3dc0ebc
SRS AD <artaksergievich@gmail.com>
2020-04-15 12:42:03 +0300

    update: (auth.controller.js) базовая авторизация

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -5,7 +5,6 @@ const User = require('../models/user.model')
 
 module.exports.login = async (req, res) => {
   const candidate = await User.findOne({ login: req.body.login })
-  console.log('candidate', candidate)
   if (candidate) {
     // проверка пароля
     const isCorrect = bcrypt.compareSync(req.body.password, candidate.password)
@@ -13,18 +12,28 @@ module.exports.login = async (req, res) => {
       const token = jwt.sign(
         {
           userId: candidate._id,
-          login: candidate.login
+          login: candidate.login,
+          roleId: candidate.role
+          // fingerprint: req.body.fingerprint
         },
         process.env.JWT,
         // секунды * минуты * часы * дни
-        { expiresIn: 60 * 60 * 24 * 2 }
+        // { expiresIn: 60 * 60 * 24 * 2 }
+        { expiresIn: 60 * 20 }
       )
 
-      res.status(200).json({
-        token,
-        userId: candidate._id
-        // role: candidate.role
-      })
+      const refreshToken = jwt.sign(
+        {
+          userId: candidate._id,
+          login: candidate.login,
+          roleId: candidate.role
+          // fingerprint: req.body.fingerprint
+        },
+        process.env.JWT,
+        { expiresIn: 60 * 60 }
+      )
+
+      res.status(200).json({ token, refreshToken })
     } else {
       res.status(401).json({ message: 'Неверный логин или пароль' })
     }
