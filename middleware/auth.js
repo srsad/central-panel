acd3d8d532e3c661e544924a2766902427bb5602
SRS AD <artaksergievich@gmail.com>
2020-04-16 18:58:01 +0300

    update: добавил проверку на странице авторизации

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,10 +1,5 @@
-export default function({ store, redirect, route }) {
+export default function({ store, redirect }) {
   if (!store.getters['auth/isAuthenticated']) {
     redirect('/auth')
-  } else if (
-    !store.getters['auth/isAuthenticated'] &&
-    route.fullPath === '/auth'
-  ) {
-    redirect('/')
   }
 }
