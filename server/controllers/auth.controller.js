4ac350bd6623ac65ac3939a3cbb232d2dd24d634
SRS AD <artaksergievich@gmail.com>
2020-04-17 12:00:46 +0300

    update: (auth.controller.js) привязал сессии И добавил проверки

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -2,51 +2,66 @@ require('dotenv').config()
 const bcrypt = require('bcrypt-nodejs')
 const jwt = require('jsonwebtoken')
 const User = require('../models/user.model')
+const Session = require('../models/session.model')
 
 module.exports.login = async (req, res) => {
-  const candidate = await User.findOne(
-    {
+  try {
+    const candidate = await User.findOne({
       login: req.body.login
-    },
-    { password: -1 }
-  ).populate('role', { access: 1 })
-  // TODO проверка статуса пользователя
-  if (candidate) {
-    // проверка пароля
-    const isCorrect = bcrypt.compareSync(req.body.password, candidate.password)
-    if (isCorrect === true) {
-      const token = jwt.sign(
-        {
-          userId: candidate._id,
-          login: candidate.login,
-          roleId: candidate.role._id
-          // fingerprint: req.body.fingerprint
-        },
-        process.env.JWT,
-        // секунды * минуты * часы * дни
-        // { expiresIn: 60 * 60 * 24 * 2 }
-        { expiresIn: 60 * 20 }
+    }).populate('role', { access: 1 })
+
+    if (!candidate.active) {
+      res.status(403).json({ message: 'Вы заблокированы администратором!' })
+    }
+
+    if (candidate) {
+      // проверка пароля
+      const isCorrect = bcrypt.compareSync(
+        req.body.password,
+        candidate.password
       )
+      if (isCorrect === true) {
+        const token = jwt.sign(
+          {
+            userId: candidate._id,
+            login: candidate.login,
+            roleId: candidate.role._id,
+            fingerprint: req.body.fingerprint
+          },
+          process.env.JWT,
+          // секунды * минуты * часы * дни
+          // { expiresIn: 60 * 60 * 24 * 2 }
+          // { expiresIn: 60 * 20 }
+          { expiresIn: 20 }
+        )
 
-      // в качестве refreshToken будет id записи в таблице сессий
-      // const refreshToken = jwt.sign(
-      //   {
-      //     userId: candidate._id,
-      //     login: candidate.login,
-      //     roleId: candidate.role
-      //     // fingerprint: req.body.fingerprint
-      //   },
-      //   process.env.JWT,
-      //   { expiresIn: 60 * 60 }
-      // )
+        const session = new Session({
+          user_id: candidate._id,
+          fingerprint: req.body.fingerprint,
+          os: req.body.os,
+          browser: req.body.browser,
+          expired: new Date(new Date().getTime() + 60000),
+          // expired: new Date(new Date().getTime() + 60000),
+          token
+        })
+        await session.save()
 
-      const refreshToken = null
-      res.status(200).json({ token, refreshToken })
+        // в качестве refreshToken будет id записи в таблице сессий
+        const user = JSON.parse(JSON.stringify(candidate))
+        delete user.password
+        res.status(200).json({ token, refreshToken: session._id, user })
+      } else {
+        res.status(404).json({ message: 'Неверный логин или пароль' })
+      }
     } else {
+      // Пользователь не найден
       res.status(404).json({ message: 'Неверный логин или пароль' })
     }
-  } else {
-    // Пользователь не найден
-    res.status(404).json({ message: 'Неверный логин или пароль' })
+  } catch (error) {
+    console.error(
+      'auth.controller - login. Неудачная попытка авторизации.',
+      error
+    )
+    res.status(404).json({ message: 'Неудачная попытка авторизации' })
   }
 }
