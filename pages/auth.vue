09a895c1be0722350e9af98a8cfe66f61917a228
SRS AD <artaksergievich@gmail.com>
2020-04-17 12:08:29 +0300

    update: (auth.vue) отправка данных для авторизации + Полностью готово

--- pages/auth.vue
+++ pages/auth.vue
@@ -88,33 +88,23 @@ export default {
       const fingerprint = await this.getFingerprint()
       try {
         const formData = this.form
-        formData.fingerprint = fingerprint
+        formData.fingerprint = String(
+          Fingerprint2.x64hash128(fingerprint.join(''), 31)
+        )
+        formData.os = fingerprint[16]
+        formData.browser = fingerprint[0]
         await this.$store.dispatch('auth/login', formData)
         this.$router.push('/')
       } catch (e) {
-        //
+        console.error('Ошибка формы авторизации', e)
       } finally {
         this.loading = false
       }
     },
     async getFingerprint() {
-      const options = {
-        excludes: {
-          plugins: true,
-          localStorage: true,
-          adBlock: true,
-          screenResolution: true,
-          availableScreenResolution: true,
-          enumerateDevices: true,
-          pixelRatio: true,
-          doNotTrack: true
-        }
-      }
-
       try {
-        const components = await Fingerprint2.getPromise(options)
-        const values = components.map((component) => component.value)
-        return String(Fingerprint2.x64hash128(values.join(''), 31))
+        const components = await Fingerprint2.getPromise({})
+        return components.map((component) => component.value)
       } catch (e) {
         console.error('getHash', e)
       }
