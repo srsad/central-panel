810462392419d8ada99a04b27241ce1d5495eca6
SRS AD <artaksergievich@gmail.com>
2020-04-22 04:35:27 +0300

    chore: (Panel.vue) + .mb-30

--- components/recording/for-not-recording/Panel.vue
+++ components/recording/for-not-recording/Panel.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="row">
-    <div class="col-6">
+    <div class="col-6 mb-30">
       <h3 class="mt-10">Tехника не для записи</h3>
     </div>
     <div class="col-6 text-right mt-10 mb-30">
