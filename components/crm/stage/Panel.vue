4cd8ffa5b5de842502a0b29f7d76e0e6b3757223
srsad <artaksergievich@gmail.com>
2020-09-09 23:27:24 +0300

    fix: отчистил панель для успешной сборки

---
+++ components/crm/stage/Panel.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="row">
+    <div class="col-12">
+      <!-- <el-button
+        v-if="$abilities('crm-settings-status-create')"
+        @click="
+          $store.commit('settings/SWITCH_DRAWNER', {
+            dranwer: 'drawerCRMCreateStatus',
+            status: true
+          })
+        "
+        size="mini"
+        type="success"
+        class="mb-20"
+        icon="el-icon-plus"
+      >
+        Добавить статус
+      </el-button> -->
+
+      Этапы
+    </div>
+  </div>
+</template>
