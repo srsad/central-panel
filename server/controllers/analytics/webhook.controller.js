589a056a1365cf031f12bea8083eecbae9c1b828
srsad <artaksergievich@gmail.com>
2020-11-04 17:44:29 +0300

    [alfa] fix: вывод конкретных ошибок

--- server/controllers/analytics/webhook.controller.js
+++ server/controllers/analytics/webhook.controller.js
@@ -12,7 +12,7 @@ module.exports.create = async (req, res) => {
     await Webhook.create(fd)
     res.status(201).json({ message: 'Bебхук обработан!' })
   } catch (e) {
-    res.status(500).json({ message: 'Не удалось обработать, вебхук!', error: e })
+    res.status(500).json({ message: 'Не удалось обработать, вебхук!', error: e.request })
   }
 }
 
@@ -24,7 +24,7 @@ module.exports.getAll = async (req, res) => {
     const webhooks = await Webhook.find().sort({ created: -1 })
     res.json({ data: webhooks })
   } catch (error) {
-    res.status(500).json({ message: 'Не удалось получить список ролей!' })
+    res.status(500).json({ message: 'Не удалось получить список ролей!', error: error.request })
   }
 }
 
