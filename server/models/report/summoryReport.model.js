0c8fc9390a0ff16a20745f9fa2541a4ecc3b07f4
srsad <artaksergievich@gmail.com>
2020-11-23 20:07:10 +0300

    update: добавил возможность работать из одного отчета с множеством филиалов

--- server/models/report/summoryReport.model.js
+++ server/models/report/summoryReport.model.js
@@ -2,14 +2,14 @@
  * Отчеты - сводная таблица
  * @typedef ReportSummory - Объект с данными по бренду
  * @property {string} name.required - Наименование отчета
- * @property {string} brands_id.required - id бренда, ссылка на коллекцию report_brands
- * @property {string} branch_id.required - id филиала, ссылка на коллекцию report_branches
+ * @property {Array.<string>} brands_id.required - id брендов, ссылка на коллекцию report_brands
+ * @property {Array.<string>} branch_id.required - id филиалов, ссылка на коллекцию report_branches
  * @property {string} period.required - период в формате (YYYY-MM-DD YYYY-MM-DD)
  * @property {Brands.model} brands.required - массив со списком брендов и его сводными данными
+ * @property {Total.model} total.required - объект с итогом
  */
 
 const { model, Schema } = require('mongoose')
-
 
 const summoryReportSchema = new Schema({
   name: {
@@ -24,23 +24,30 @@ const summoryReportSchema = new Schema({
       require: true
     }
   ],
-  branch_id: {
-    type: Schema.Types.ObjectId,
-    ref: 'report_branches',
-    require: true
-  },
+  branch_id: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'report_branches',
+      require: true
+    }
+  ],
   period: {
     type: String,
     require: true
   },
+  // массив с данными по брендам
   brands: [
     {
+      // id бренда
       brand: {
         type: Schema.Types.ObjectId,
         ref: 'report_brands'
       },
-      // brand_name
-      // бенем из report_brands
+      // id филиала
+      branch: {
+        type: Schema.Types.ObjectId,
+        ref: 'report_branches'
+      },
       // заявки
       requests: {
         // канал
@@ -199,6 +206,162 @@ const summoryReportSchema = new Schema({
       }
     }
   ],
+  // итог
+  total: {
+    type: Object,
+    default: {
+      // заявки
+      requests: {
+        // канал
+        pkSeo: {
+          type: Number,
+          default: 0
+        },
+        // цена за трафик
+        traffik_price: {
+          type: Number,
+          default: 0
+        },
+        // общая
+        common_price: {
+          type: Number,
+          default: 0
+        }
+      },
+      // Запись
+      order: {
+        // кол-во
+        count: {
+          type: Number,
+          default: 0
+        },
+        // цена за трафик
+        traffik_price: {
+          type: Number,
+          default: 0
+        },
+        // цена общая
+        common_price: {
+          type: Number,
+          default: 0
+        },
+        // конверсия %
+        conversion: {
+          type: Number,
+          default: 0
+        }
+      },
+      // пришел в СЦ
+      came_to_sc: {
+        // кол-во
+        count: {
+          type: Number,
+          default: 0
+        },
+        // цена за трафик
+        traffik_price: {
+          type: Number,
+          default: 0
+        },
+        // цена общая
+        common_price: {
+          type: Number,
+          default: 0
+        },
+        // конверсия %
+        conversion: {
+          type: Number,
+          default: 0
+        }
+      },
+      // клиент закрыт
+      order_closed: {
+        // кол-во
+        count: {
+          type: Number,
+          default: 0
+        },
+        // цена за трафик
+        traffik_price: {
+          type: Number,
+          default: 0
+        },
+        // цена общая
+        common_price: {
+          type: Number,
+          default: 0
+        },
+        // конверсия %
+        conversion: {
+          type: Number,
+          default: 0
+        }
+      },
+      // Расходы
+      common_expenses: {
+        // Баланс
+        balance: {
+          type: Number,
+          default: 0
+        },
+        // РК
+        pk: {
+          type: Number,
+          default: 0
+        },
+        // СЕО
+        seo: {
+          type: Number,
+          default: 0
+        },
+        // Общие, алока и т.д
+        common: {
+          type: Number,
+          default: 0
+        }
+      },
+      // Выручка
+      revenue: {
+        type: Number,
+        default: 0
+      },
+      // Расходы
+      expenses: {
+        type: Number,
+        default: 0
+      },
+      // Вал
+      val: {
+        type: Number,
+        default: 0
+      },
+      // Заказы
+      orders: {
+        type: Number,
+        default: 0
+      },
+      // Ср. чек
+      wed_check: {
+        type: Number,
+        default: 0
+      },
+      // Дельта
+      delta: {
+        type: Number,
+        default: 0
+      },
+      // Прибыль
+      profit: {
+        type: Number,
+        default: 0
+      },
+      // СПЗ
+      spz: {
+        type: Number,
+        default: 0
+      }
+    }
+  },
   created: {
     type: Date,
     default: Date.now
@@ -213,6 +376,7 @@ module.exports = model('summory_reports', summoryReportSchema)
 /**
  * @typedef Brands - Объект с данными по бренду
  * @property {string} brand.required - id бренда из коллекции report_brands
+ * @property {string} branch.required - id филиала из коллекции  report_branches
  * @property {Requests.model} requests - Заявки
  * @property {CommonOrder.model} order - Запись
  * @property {CommonOrder.model} came_to_sc - пришел в СЦ
@@ -255,4 +419,21 @@ module.exports = model('summory_reports', summoryReportSchema)
  * @property {integer} pk - реклама
  * @property {integer} seo - сео
  * @property {integer} common - общие
+ */
+
+/**
+ * @typedef Total - Итог
+ * @property {Requests.model} requests - Заявки
+ * @property {CommonOrder.model} order - Запись
+ * @property {CommonOrder.model} came_to_sc - пришел в СЦ
+ * @property {CommonOrder.model} order_closed - клиент закрыт
+ * @property {CommonExpenses.model} common_expenses - Расходы
+ * @property {integer} revenue - выводы - выручка
+ * @property {integer} expenses - выводы - расходы
+ * @property {integer} val - выводы - вал
+ * @property {integer} orders - выводы - заказы
+ * @property {integer} wed_check - выводы - ср. чек
+ * @property {integer} delta - выводы - дельта
+ * @property {integer} profit - выводы - прибыль
+ * @property {integer} spz - выводы - СПЗ
  */
