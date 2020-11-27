db1570916bff820cb3bc1bf3e846a7571c4a15ac
srsad <artaksergievich@gmail.com>
2020-11-28 00:59:17 +0300

    chore: немного подкорриектировал настройки свагера

--- server/docs/swagger.js
+++ server/docs/swagger.js
@@ -1,31 +1,28 @@
-const app = require('../app');
-const expressSwagger = require('express-swagger-generator')(app);
+const app = require('../app')
+const expressSwagger = require('express-swagger-generator')(app)
 
 let options = {
-    swaggerDefinition: {
-        info: {
-            description: 'Документация от центральной панели сервисного центра',
-            title: 'Service center',
-            version: '1.0.0',
-        },
-        host: process.env.BASE_URL || 'localhost:3000',
-        basePath: '/v1',
-        produces: [
-            "application/json",
-            "application/xml"
-        ],
-        schemes: ['http', 'https'],
-        securityDefinitions: {
-            JWT: {
-                type: 'apiKey',
-                in: 'header',
-                name: 'Authorization',
-                description: "",
-            }
-        }
+  swaggerDefinition: {
+    info: {
+      description: 'Документация от центральной панели сервисного центра',
+      title: 'Service center',
+      version: '1.0.0'
     },
-    basedir: __dirname, // абсолютный путь
-    files: ['../routes/**/*.js', '../models/**/*.js'] // откуда собирать документацию
-};
+    host: process.env.BASE_URL || 'localhost:3000',
+    basePath: '',
+    produces: [ 'application/json', 'application/xml' ],
+    schemes: [ 'http', 'https' ],
+    securityDefinitions: {
+      JWT: {
+        type: 'apiKey',
+        in: 'header',
+        name: 'Authorization',
+        description: ''
+      }
+    }
+  },
+  basedir: __dirname, // абсолютный путь
+  files: [ '../routes/**/*.js', '../models/**/*.js' ] // откуда собирать документацию
+}
 
 module.exports = expressSwagger(options)
