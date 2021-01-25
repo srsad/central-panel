12514c17ecb29c02fc9b852246709aacc0c391a2
srsad <artaksergievich@gmail.com>
2021-01-25 21:33:45 +0300

    [ag-grid] fix: поправил работу socket.io

--- plugins/socket.js
+++ plugins/socket.js
@@ -5,7 +5,7 @@ export default function({ store }) {
   Vue.use(
     new VueSocketIO({
       debug: false,
-      connection: 'http://localhost:3000',
+      connection: process.env.BASE_URL || 'http://localhost:3000',
       vuex: {
         store,
         actionPrefix: 'SOCKET_',
