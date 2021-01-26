3b41c99ace15f6ca7ff67429fc68fa4caa63b7f6
srsad <artaksergievich@gmail.com>
2021-01-26 21:32:50 +0300

    feature: + список адрес => шорткод, dcod => шорткод

---
+++ utils/brancheCodes.js
@@ -0,0 +1,19 @@
+/**
+ * Шорткод по адресу
+ */
+module.exports.shortCodeForAddress = new Map([
+  ['Красноармейская', 'KRS'],
+  ['Дмитровский', 'DMR'],
+  ['Новослободская', 'NVS'],
+  ['Сеславинская', 'SES']
+])
+
+/**
+ * Шорткод по коду
+ */
+module.exports.shortCodeForCodes = new Map([
+  ['01', 'KRS'],
+  ['02', 'DMR'],
+  ['03', 'NVS'],
+  ['04', 'SES']
+])
