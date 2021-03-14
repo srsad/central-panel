804607d4c4b5868a6ebe22311643fb1387571e96
srsad <artaksergievich@gmail.com>
2021-03-14 21:40:57 +0300

    feature подключил план для менеджеров

--- components/manager/Panel.vue
+++ components/manager/Panel.vue
@@ -1,11 +1,14 @@
 <template>
   <div>
     <el-tabs value="plan" type="border-card">
-      <el-tab-pane label="План" :lazy="true" name="plan">
+      <el-tab-pane :lazy="true" label="План" name="plan">
         <app-plan :branch="branch" />
       </el-tab-pane>
-      <el-tab-pane label="Расписание" :lazy="true" name="schedule">
+      <el-tab-pane :lazy="true" label="Расписание" name="schedule">
         <app-schedule :branch="branch" />
+      </el-tab-pane>
+      <el-tab-pane :lazy="true" label="Менеджмент" name="managment">
+        <app-manager :branch="branch" />
       </el-tab-pane>
     </el-tabs>
   </div>
@@ -14,17 +17,19 @@
 <script>
 import AppSchedule from '~/components/manager/schedule/Panel'
 import AppPlan from '~/components/manager/plan/Panel'
+import AppManager from '~/components/manager/manager/Panel'
 
 export default {
+  components: {
+    AppSchedule,
+    AppPlan,
+    AppManager
+  },
   props: {
     branch: {
       type: String,
       default: '33038'
     }
-  },
-  components: {
-    AppSchedule,
-    AppPlan
   }
 }
 </script>
---
+++ components/manager/manager/Panel.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="row">
+    <!-- v-if="$abilities('report-spb_33038_panel_plan_panel-create')" -->
+    <div class="col-12">
+      план
+    </div>
+  </div>
+</template>
